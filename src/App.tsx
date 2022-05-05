ef6fa0c71bfc6b66bc3e4dfc5bc4aa9681aaf585
Yappydoor <cheder2002@gmail.com>
2022-05-05 19:27:35 +0300

    bar-button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { MainPage } from './pages/main-page/main-page';
 import './normalize.css';
 import './Css/NavigationBar.css';
+import './Css/SOSMenu.css'; 
 import "@fontsource/istok-web";
 
 function App() {
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,8 +1,10 @@
 import { NavigationBar } from "../../elements/Bars/NavigationBar";
+import { SOSMenu } from "../../elements/Menus/SOSMenu";
 
 export const MainPage = () => {
     return (
         <div>
+            <SOSMenu />
             <NavigationBar />
         </div>
     );
