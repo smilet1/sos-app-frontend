889244afa6b6e3eb758612d078b7250eec01e2f5
Toradon <chelyabin.dima@gmail.com>
2022-05-12 15:15:36 +0300

    Cancel button style

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,10 +1,13 @@
 import { NavigationBarSection } from "../../components/navigation-bar-section/navigation-bar-section";
+import { CancelButton } from "../../elements/cancel-button/cancel-button";
+import { StyleMain } from "./main-page.styles";
 
 
 export const MainPage = () => {
     return (
-        <div>
-            <NavigationBarSection />
-        </div>
+        <StyleMain>
+            <CancelButton />
+         </StyleMain>
+
     );
 }
